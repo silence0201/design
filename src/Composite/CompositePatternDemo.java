3fb482f7c587b18c049c5ae9112e67284abdc506
Silence <374619540@qq.com>
2016-07-29 23:10:06 +0800

    Add CompositePatternDemo

---
+++ src/Composite/CompositePatternDemo.java
@@ -0,0 +1,9 @@
+package Composite;
+
+/**
+ * Description: CompositePatternDemo
+ * Author: silence
+ * Update: silence(2016-07-29 23:07)
+ */
+public class CompositePatternDemo {
+}
---
+++ src/Composite/Employee.java
@@ -0,0 +1,9 @@
+package Composite;
+
+/**
+ * Description: Employee
+ * Author: silence
+ * Update: silence(2016-07-29 23:06)
+ */
+public class Employee {
+}
