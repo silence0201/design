8a289002e7304fe95861d05fb73858afbd3a79ec
Silence <374619540@qq.com>
2016-08-01 20:24:56 +0800

    Add MediatorPatternDemo

---
+++ src/Mediator/ChatRoom.java
@@ -0,0 +1,9 @@
+package Mediator;
+
+/**
+ * Description: ChatRoom
+ * Author: silence
+ * Update: silence(2016-08-01 20:23)
+ */
+public class ChatRoom {
+}
---
+++ src/Mediator/MediatorPatternDemo.java
@@ -0,0 +1,9 @@
+package Mediator;
+
+/**
+ * Description: MediatorPatternDemo
+ * Author: silence
+ * Update: silence(2016-08-01 20:24)
+ */
+public class MediatorPatternDemo {
+}
